a3a861aa2b6a31dcc633809a34f7c9eee6f330e3
Sam Scarpello <Sam.Scarpello@capgroup.com>
2020-10-28 07:55:30 -0700

    initial revision

---
+++ src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import io.prometheus.client.GaugeMetricFamily;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/metrics")
+	public String index() {
+		GaugeMetricFamily gauge = new GaugeMetricFamily("consyss_custom_metric",
+              "Sample Metric.  Goes from 0 to 1",
+              0.6F);
+		return gauge.toString();
+	}
+
+}
